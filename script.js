2d09a7b6b78a48caab70d92a77914702fd9e5486
Gabriel Dias <gabds10@gmail.com>
2024-09-14 18:34:23 -0300

    Update de cumprimento

--- script.js
+++ script.js
@@ -3,19 +3,18 @@ function carregar() {
     var img = window.document.getElementById('imagem')
     var data = new Date()
     var hora = data.getHours()
-
-    msg.innerHTML = `Agora são ${hora} horas.`
+  
     if (hora >= 0 && hora <12) {
-        // bom dia!
         img.src = 'morning2.png'
+        msg.innerHTML = `Bom dia! <p> Agora são ${hora} horas.`
         document.body.style.background = '#a04801'
     } else if (hora >= 12 && hora <= 18) {
-        // boa tarde!
         img.src = 'lunch2.png'
+        msg.innerHTML = `Boa tarde! <p> Agora são ${hora} horas.`
         document.body.style.background = '#dec3b0'
     } else {
-        // boa noite
         img.src = 'dinner2.png'
+        msg.innerHTML = `Boa noite! <p> Agora são ${hora} horas.`
         document.body.style.background = '#56361d'
     }
 }
